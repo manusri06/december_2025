7746888798ed8394d73f38629e0acce899860a6e
V. Manusri <velpula.manusri06@gmail.com>
2025-12-10 19:38:45 +0530

    Time: 2 ms (55.79%), Space: 18 MB (30.53%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -1,14 +1,32 @@
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
-        temp = " "
-        i = 0
-        al = len(a)
-        bl = len(b)
-        limit = (bl//al)+1
-        while b not in temp:
-            if i<=limit:
-                i += 1
-                temp = a*i
-            else:
-                return -1
-        return i
+
+        s = a
+        i = 1
+
+        while len(s) < len(b):
+            s+=a
+            i+=1
+
+        if b in s:
+            return i
+
+        s = s+a
+        i+=1
+        if b in s:
+            return i
+            
+        return -1
+
+"""temp = " "
+i = 0
+al = len(a)
+bl = len(b)
+limit = (bl//al)+1
+while b not in temp:
+    if i<=limit:
+        i += 1
+        temp = a*i
+    else:
+        return -1
+return i """
